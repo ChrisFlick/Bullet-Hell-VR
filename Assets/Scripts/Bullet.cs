f32a65062dad70919de046a4ace5175c12d6caf9
Chris Flick <chrisflick@gmail.com>
2024-05-15 14:11:08 -0700

    Catching a bullet now changes it to a blue material

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,9 +8,13 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _bulletSpeed = 10f;
     [SerializeField] private float _killDistance = 20f;
 
+    [SerializeField] private Material _caughtMaterial;
+
     private Vector3 _direction;
 
     private Vector3 _destroyLocation;
+
+    bool _isCaught = false;
 
     private void Start()
     {   
@@ -22,7 +26,9 @@ public class Bullet : MonoBehaviour
     }
 
     private void Update()
-    {   
+    {
+        if (_isCaught) return;
+
         transform.Translate(_direction * _bulletSpeed * Time.deltaTime);
 
         float distance = Vector3.Distance(transform.position, _destroyLocation);
@@ -31,5 +37,17 @@ public class Bullet : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void OnSelectEnter()
+    {
+        // Used to stop the movement when the Bullet is caught
+        _isCaught = true;
+
+        Renderer renderer = GetComponent<Renderer>();
+
+        var materialsCopy = renderer.materials;
+        materialsCopy[0] = _caughtMaterial;
+        GetComponent<Renderer>().materials = materialsCopy;
     }
 }
