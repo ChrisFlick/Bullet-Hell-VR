3bb5d8ce98abe518747712e7c4689b0b7a3189bd
Chris Flick <chrisflick@gmail.com>
2024-05-17 13:35:10 -0700

    Game now loops back to Main Menu after a minute of gameplay

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,28 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    const float GAME_LENGTH = 60;
+
     [SerializeField] private Transform _flyingSaucerPrefab;
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private float _spawnTime;
 
     private float _timeSinceLastSpawn = 0;
+    private float _gameTime = 0;
 
     private int score = 0;
 
-    private bool _isActive;
 
     private void Start()
     {
         FlyingSaucer.OnAnyHit += FlyingSaucer_OnAnyHit;
         BulletKillTrigger.OnAnyDodged += BulletKillTrigger_OnAnyDodged;
+
+        _timeSinceLastSpawn = _spawnTime; // This ensures a Flying Saucer spawns at the start of the game.
     }
 
     private void Update()
     {
-        if (!_isActive) return;
+        _gameTime += Time.deltaTime;
+
+        if (_gameTime > GAME_LENGTH)
+        {
+            int mainMenuIndex = 0;
+            SceneManager.LoadScene(mainMenuIndex, LoadSceneMode.Single);
+        }
 
         _timeSinceLastSpawn += Time.deltaTime;
 
@@ -38,7 +49,6 @@ public class GameController : MonoBehaviour
 
     private void FlyingSaucer_OnAnyHit(object sender, EventArgs e)
     {
-        if (!_isActive) return;
 
         score += 1000;
 
@@ -47,7 +57,6 @@ public class GameController : MonoBehaviour
 
     private void BulletKillTrigger_OnAnyDodged(object sender, EventArgs e)
     {
-        if (!_isActive) return;
 
         // Add to score based on how many bullets are dodged.
         score += 1;
